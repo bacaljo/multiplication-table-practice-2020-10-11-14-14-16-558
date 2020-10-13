ee862d28c9f722a564cb32909942d1a94fa47db9
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-13 16:07:12 +0800

    [Multiplication Table] [John Emmanuel] feature: create empty methods isNumberWithinRange and isFirstNumberLarger to complete logic of validateInput

--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -12,6 +12,18 @@ public class MultiplicationTable {
     }
 
     private boolean validateInput(int startNumber, int endNumber) {
+        boolean isFirstNumberLarger = isFirstNumberLarger(startNumber, endNumber);
+        boolean isFirstNumberWithinRange = isNumberWithinRange(startNumber, 1, 1000);
+        boolean isSecondNumberWithinRange = isNumberWithinRange(endNumber, 1, 1000);
+
+        return !isFirstNumberLarger && isFirstNumberWithinRange && isSecondNumberWithinRange;
+    }
+
+    private boolean isNumberWithinRange(int startNumber, int lowerLimit, int upperLimit) {
+        return false;
+    }
+
+    private boolean isFirstNumberLarger(int startNumber, int endNumber) {
         return false;
     }
 }
