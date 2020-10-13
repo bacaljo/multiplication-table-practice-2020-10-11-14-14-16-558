56398e30012ac01971a1f15659be8a70b1e2c512
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-13 16:41:34 +0800

    [Multiplication Table] [John Emmanuel] fix: correct generateMultiplicationExpression call

--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -19,7 +19,7 @@ public class MultiplicationTable {
     private String generateMultiplicationLine(int num1, int num2) {
         String[] multiplicationExpressions = new String[(num2 - num1) + 1];
         for (int i = 0; i < multiplicationExpressions.length; i++) {
-            multiplicationExpressions[i] = generateMultiplicationExpression(num1, num2 + i);
+            multiplicationExpressions[i] = generateMultiplicationExpression(num1 + i, num2);
         }
 
         return String.join("  ", multiplicationExpressions);
