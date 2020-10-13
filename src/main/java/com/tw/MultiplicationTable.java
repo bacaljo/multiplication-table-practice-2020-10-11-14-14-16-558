ce04d6f0218f61e63b5e5cd4773eb8e7df0462c0
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-13 16:05:57 +0800

    [Multiplication Table] [John Emmanuel] feature: create empty methods generateMultiplicationTable and validateInput to complete logic of create

--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -2,6 +2,16 @@ package com.tw;
 
 public class MultiplicationTable {
     public String create(int startNumber, int endNumber) {
+        return validateInput(startNumber, endNumber)
+                ? generateMultiplicationTable(startNumber, endNumber)
+                : null;
+    }
+
+    private String generateMultiplicationTable(int startNumber, int endNumber) {
         return null;
+    }
+
+    private boolean validateInput(int startNumber, int endNumber) {
+        return false;
     }
 }
